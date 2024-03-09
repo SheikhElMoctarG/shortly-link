a8ce261d5de567440407c2d62af838dabdf87fde
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-03-09 15:19:38 +0000

    finish index page

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="fixed top-0 right-0 left-0 border-white/20 border-b">
+  <div class="fixed top-0 right-0 left-0 border-white/20 border-b backdrop-blur-md">
     <nav class="container py-4 flex justify-between items-center">
       <NuxtLink to="/" class="text-2xl font-bold">Shortly-Link</NuxtLink>
       <ul class="flex gap-4 items-center ">
         <li>Price</li>
         <li>Price</li>
-        <button class="bg-yellow-700 px-4 py-2 rounded-full text-white">Sign Up</button>
+        <button class="btn-yellow">Sign In</button>
       </ul>
     </nav>
   </div>
