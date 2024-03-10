305a915016e3d65bb9d16d67a9f85c598150d4d6
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-03-10 13:31:55 +0000

    add font

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="fixed top-0 right-0 left-0 border-white/20 border-b backdrop-blur-md">
+  <div class="fixed top-0 right-0 left-0 border-white/20 border-b backdrop-blur-md z-50">
     <nav class="container py-4 flex justify-between items-center">
       <NuxtLink to="/" class="text-2xl font-bold">Shortly-Link</NuxtLink>
       <ul class="flex gap-4 items-center ">
-        <li>Price</li>
-        <li>Price</li>
+        <li>About us</li>
+        <li>Our services</li>
         <NuxtLink to="/auth" class="btn-yellow">Sign In</NuxtLink>
       </ul>
     </nav>
