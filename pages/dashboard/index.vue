3688516e2f694caa3890e8e20ed10ad850e7f0bc
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-03-11 05:55:08 +0000

    connect to database and create an new short key

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -6,14 +6,14 @@
     </section>
     <!-- section two  -->
     <section class="mycard my-3">
-      <form class="flex  gap-4">
+      <form class="flex  gap-4" @submit.prevent="createShortKey()">
         <div class="w-5/12"> 
             <label for="orginalURL">orginal URL</label>
-            <input type="text" name="orginalURL" id="orginalURL" placeholder="your url ">
+            <input type="text" name="orginalURL" id="orginalURL" placeholder="your url " v-model="form.orginal_url">
           </div>
           <div class="w-5/12">
             <label for="shortKey">orginal URL</label>
-            <input type="text" name="shortKey" id="shortKey" placeholder="short key ">
+            <input type="text" name="shortKey" id="shortKey" placeholder="short key " v-model="form.short_key">
           </div>
           <div class="w-full md:w-2/12 flex justify-end self-end">
           <button type="submit" class="btn-yellow w-full">Shorten <Icon name="material-symbols-light:link-rounded" size="25"/></button>
@@ -28,9 +28,34 @@
 </template>
 
 <script lang="ts" setup>
+import { nanoid } from 'nanoid';
+import type { Database } from '../../types/supabase';
+const client = useSupabaseClient<Database>();
 definePageMeta({
   middleware: 'auth'
-})
+});
+const form = reactive({
+  orginal_url: '',
+  short_key: ''
+});
+onMounted(()=> {
+  form.short_key = nanoid(6);
+});
+
+async function createShortKey() {
+  const {data, error} = await client.from('links').insert({
+      short_key: form.short_key,
+      orginal_url: form.orginal_url,
+      user_id: useSupabaseUser().value?.id,
+      total_clicks: 0
+    });
+    if(error){
+      alert(error.message);
+      return;
+    } 
+    alert('created successfully')
+}
+
 </script>
 
 <style>
---
+++ types/supabase.ts
@@ -0,0 +1,171 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export type Database = {
+  public: {
+    Tables: {
+      clicks: {
+        Row: {
+          city: string | null
+          country: string | null
+          created_at: string
+          id: number
+          ip: string | null
+          link_id: number
+          user_agient: string
+        }
+        Insert: {
+          city?: string | null
+          country?: string | null
+          created_at?: string
+          id?: number
+          ip?: string | null
+          link_id: number
+          user_agient: string
+        }
+        Update: {
+          city?: string | null
+          country?: string | null
+          created_at?: string
+          id?: number
+          ip?: string | null
+          link_id?: number
+          user_agient?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "public_clicks_link_id_fkey"
+            columns: ["link_id"]
+            isOneToOne: true
+            referencedRelation: "links"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      links: {
+        Row: {
+          created_at: string
+          id: number
+          orginal_url: string | null
+          short_key: string
+          total_clicks: number | null
+          user_id: string | null
+        }
+        Insert: {
+          created_at?: string
+          id?: number
+          orginal_url?: string | null
+          short_key: string
+          total_clicks?: number | null
+          user_id?: string | null
+        }
+        Update: {
+          created_at?: string
+          id?: number
+          orginal_url?: string | null
+          short_key?: string
+          total_clicks?: number | null
+          user_id?: string | null
+        }
+        Relationships: []
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (Database["public"]["Tables"] & Database["public"]["Views"])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (Database["public"]["Tables"] &
+      Database["public"]["Views"])
+  ? (Database["public"]["Tables"] &
+      Database["public"]["Views"])[PublicTableNameOrOptions] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : never
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : never
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : never
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof Database["public"]["Enums"]
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
+  ? Database["public"]["Enums"][PublicEnumNameOrOptions]
+  : never
