6d50d0fee1fa686a3f282f9a78b50c0e1183d3db
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-03-11 03:42:49 +0000

    add login page and register page

---
+++ components/RegisterSection.vue
@@ -0,0 +1,40 @@
+<template>
+  <form @submit.prevent="registerWithEmail()">
+    <div>
+      <label for="email">Email</label>
+      <input type="email" name="email" id="email" placeholder="yourname@example.com" v-model="form.email">
+    </div>
+    <div>
+      <label for="password">Password</label>
+      <input type="password" name="password" id="password" v-model="form.pass">
+    </div>
+    <button type="submit" class="btn-yellow mt-2 w-full">Register</button>
+    <p class="text-center mt-3 text-red-500">{{ myerror }}</p>
+  </form>
+</template>
+<script lang="ts" setup>
+const client = useSupabaseClient();
+const form = reactive({
+  email: '',
+  pass: ''
+});
+const myerror = ref<String>('');
+async function registerWithEmail() {
+  if (!form.email || !form.pass) {
+    return myerror.value = 'there is a problem in email or password ';
+  };
+  try {
+    const { data, error } = await client.auth.signUp({
+      email: form.email,
+      password: form.pass
+    });
+    if (error) {
+      myerror.value = error.message;
+      return;
+    }
+    console.log(data)
+  } catch (error) {
+    myerror.value = "something is wrong..."
+  }
+}
+</script>
--- pages/auth.vue
+++ pages/auth.vue
@@ -3,31 +3,29 @@
     <div class="container">
       <div class="mycard">
         <div class="flex justify-center py-2">
-          <Icon name="arcticons:otp-authenticator" size="45" class="text-white/90"/>
+          <Icon name="arcticons:otp-authenticator" size="45" class="text-white/90" />
         </div>
         <button class="btn-yellow my-4 font-bold w-full" @click="loginWithGithub">Continue using Github</button>
         <hr class="border border-white/10 my-3">
-        <form action="">
-          <div>
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" placeholder="yourname@example.com">
-          </div>
-          <div>
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" placeholder="yourname@example.com">
-          </div>
-          <button type="submit" class="btn-yellow mt-2 w-full">Login</button>
-        </form>
+        <div v-if="isLogining">
+          <LoginSection/>
+          <p class="text-center cursor-pointer" @click="isLogining = false">you dont have an one? <span>Register</span></p>
+        </div>
+        <div v-else>
+          <RegisterSection/>
+          <p class="text-center cursor-pointer" @click="isLogining = true">already have an one? <span>Login</span></p>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-const client = useSupabaseClient()
-function loginWithGithub(){
-  client.auth.signInWithOAuth({provider: 'github'});
-}
+const client = useSupabaseClient();
+const isLogining = ref<Boolean>(true);
+function loginWithGithub() {
+  client.auth.signInWithOAuth({ provider: 'github' });
+};
 </script>
 
 <style></style>
