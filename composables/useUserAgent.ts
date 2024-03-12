b18ee124d0949087c06e88961a57786d5efa9b45
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-03-12 21:56:07 +0000

    update

--- composables/useUserAgent.ts
+++ composables/useUserAgent.ts
@@ -6,26 +6,26 @@ type UserAgent = {
   export default function useUserAgent(): UserAgent | null {
     if (process.server) {
       const nuxtApp = useNuxtApp();
-      if (nuxtApp.ssrContext && nuxtApp.ssrContext.event.node.req) {
+      const req = nuxtApp.ssrContext?.event.node.req;
+      if (nuxtApp.ssrContext && req) {
         const ip =
-          nuxtApp.ssrContext.event.node.req.headers["x-forwarded-for"] ||
-          nuxtApp.ssrContext.event.node.req.connection.remoteAddress || // for local dev
-          nuxtApp.ssrContext.event.node.req.socket.remoteAddress;
+          req.headers["x-forwarded-for"] ||
+          req.socket.remoteAddress;
   
-        const ua = nuxtApp.ssrContext.event.node.req.headers["user-agent"];
+        const ua = req.headers["user-agent"];
   
         return {
           userAgent: ua || "",
-          ip: typeof ip === "string" ? ip : undefined,
+          ip: typeof ip === "string" ? ip : ip?.[0],
         };
       }
     } else {
+        console.log(process.server)
       const ua = navigator.userAgent;
       return {
-        userAgent: ua,
-        ip: undefined,
+        userAgent: ua
       };
     }
-  
+    
     return null;
   }
