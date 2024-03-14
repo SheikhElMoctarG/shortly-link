de017d71749bb4269f5a1f427af196bf5efb5c86
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2024-03-14 08:57:24 +0000

    update

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <h1 class="text-5xl font-bold text-center bg-gradient-to-b from-slate-700 to-yellow-600 text-transparent bg-clip-text lg:px-[150px]">Make your links tiny and easy to share with Shortly-Link</h1>
     <p class="my-10 text-xl font-medium text-slate-600 text-center">Transform your long, cumbersome URLs into short and shareble links </p>
     <NuxtLink to="/dashboard" class="btn-yellow text-xl font-semibold capitalize">Short your link</NuxtLink>
+    <p class="my-[15px]">By <a href="https://www.sheikhelmoctar.me" class="px-3 py-1 text-blue-700">Sheikh El-Moctar ❤️</a></p>
   </div>
 </template>
 
